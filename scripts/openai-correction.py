85be4ae30f3aa18c2950f472e6af855658c9ccfe
Prakhar Jain <prakhs123@gmail.com>
2023-01-08 20:57:55 +0530

    open ai outputs 2

--- scripts/openai-correction.py
+++ scripts/openai-correction.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import logging
+import time
+
 import openai
 import argparse
 
@@ -31,25 +33,50 @@ chunks = text.split('\n\n')
 
 for chunk in chunks:
     print(len(chunk)/4)
-    if len(chunk)/4 > 1950:
+    if len(chunk)/4 > 1000:
         print(chunk)
     print('-'*100)
 
+
+def make_request(chunk):
+    # Make request to API
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Correct this to standard English, fix OCR errors, don't add heading and output in markdown format:\n" + chunk,
+        temperature=0,
+        max_tokens=2000,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    return response
+
+
 with open(md_file_path, "a") as fout:
     for chunk in chunks:
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt="Correct this to standard English with optional heading if it is present, don't add heading unnecessary:\n"+chunk,
-            temperature=0,
-            max_tokens=2000,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0
-        )
+        max_retries = 2
+        retry_delay = 2
+
+        for i in range(max_retries):
+            try:
+                response = make_request(chunk)
+                # Request succeeded, break out of loop
+                break
+            except Exception as e:
+                if i == max_retries - 1:
+                    # Last retry, raise exception
+                    raise e
+                else:
+                    # Calculate delay for next retry
+                    retry_delay *= 2
+                    time.sleep(retry_delay)
         ans = response["choices"][0]
-        logger.info(chunk)
         if ans["finish_reason"] == "stop":
-            logger.info(ans['text'])
+            print(chunk)
+            print('-'*100)
+            print(ans['text'])
+            print('-' * 100)
             fout.write(ans['text'])
         else:
+            print(response)
             sys.exit(1)
